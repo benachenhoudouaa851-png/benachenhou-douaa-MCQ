d3bd0e938f91445c8448e76e7db397780aa3408c
benachenhou douaa MCQ <benachenhoudouaa851@gmail.com>
2025-12-11 00:09:16 +0100

    Fix import statement and format data dictionary
    Fixed import typo and corrected data formatting.

--- projet.py
+++ projet.py
@@ -4,13 +4,13 @@
 #Ayed Zeddam Abir
 #Derni Salima
 
-impoert pandas as pd
+import pandas as pd
 
 # Donner : Séquence,Longueur,Pourcentage GC
-Data: = {
-    "Séquence":  ["ATGCGTACGTA","GCTAGCTAGGCC","ATGCGCGTAAGT","TACGATCGTA","ATGAAAGGCTT","CGTACGTAGC","TTAACCGGAT"]
-    "Longueur": [12,12,12,10,11,10,10]
-    "Pourcentage GC": [50,66.67,58.33,40,45.45,60,50]
+Data = {
+    "Séquence":  ["ATGCGTACGTA", "GCTAGCTAGGCC", "ATGCGCGTAAGT", "TACGATCGTA", "ATGAAAGGCTT", "CGTACGTAGC", "TTAACCGGAT"]
+    "Longueur": [12, 12, 12, 10, 11, 10, 10]
+    "Pourcentage GC": [50, 66.67, 58.33, 40, 45.45, 60, 50]
 }
 # herir nesrine 
 # 1) Création et affichage d'une DataFrame (tableau pandans)
