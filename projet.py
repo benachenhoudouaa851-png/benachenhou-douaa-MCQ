3fd4611851a3a3209ee33ae154dda94901045acf
benachenhou douaa MCQ <benachenhoudouaa851@gmail.com>
2025-12-10 23:16:26 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -6,4 +6,9 @@
 
 impoert pandas as pd
 
-
+# Donner : Séquence,Longueur,Pourcentage GC
+Data: = {
+    "Séquence":  ["ATGCGTACGTA","GCTAGCTAGGCC","ATGCGCGTAAGT","TACGATCGTA","ATGAAAGGCTT","CGTACGTAGC","TTAACCGGAT"]
+    "Longueur": [12,12,12,10,11,10,10]
+    "Pourcentage GC": [50,66.67,58.33,40,45.45,60,50]
+}
